d6aa20908871b1906b4cb36f51bf63eb69ae7efd
lcarrafabr <lcarrafa.br@gmail.com>
2022-09-13 19:43:54 -0300

    Primeiro commit

---
+++ CamelCase1/camel-case-1/test/CamelCaseConverterTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CamelCaseConverterTest {
+	
+	private CamelCaseConverter camelCase;
+	
+	@Before //Executado sempre antes de cada um dos @Test
+	public void setup() {
+		
+		camelCase = new CamelCaseConverter();
+	}
+	
+	@Test
+	public void deveConverterNomeSimples() throws Exception {
+		
+		assertEquals("Luciano", camelCase.converter("luciano"));
+	}
+	
+	@Test
+	public void deveConverterNomeMisturadoEMaiusculoEMinusculo() throws Exception {
+		
+		assertEquals("Luciano", camelCase.converter("lUcIaNo"));
+	}
+}
